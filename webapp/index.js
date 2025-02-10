4e1207c761e8ed29bc537c3c71c7c071ad1032f1
Arbaj Shekh <arbaaz.sh001@gmail.com>
2025-02-10 11:57:20 +0000

    Working with Views and Controllers-Working with XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,16 @@
-sap.ui.define(["sap/m/Text"],function (Text){
+sap.ui.define(["sap/ui/core/mvc/XMLView"],function (XMLView){
     "use-strict"
 
+    XMLView.create({
+        id:"app",
+        viewName:"sap.training.exc.view.App"
 
-    new Text({text:"hello world"}).placeAt("content");
+    }).then(function (oView){
+        oView.placeAt("content");
+    })
+
+
+
 
     
 });
