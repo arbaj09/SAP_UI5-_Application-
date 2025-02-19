cb76b7c74a1fcb29e7e196314407d6aa69376094
Arbaj Shekh <arbaaz.sh001@gmail.com>
2025-02-19 05:52:04 +0000

    Odata_model_(create)-Implement an OData Create Operation

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,60 +1,69 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/core/syncStyleClass",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/syncStyleClass",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator",
+  "sap/m/MessageToast"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller,syncStyleClass, JSONModel, Filter, FilterOperator) {
-        "use strict";
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, syncStyleClass, JSONModel, Filter, FilterOperator, MessageToast) {
+      "use strict";
 
-        return Controller.extend("sap.training.exc.controller.Overview", {
+      return Controller.extend("sap.training.exc.controller.Overview", {
 
-            onSave: function () {
-                if (!this.pDialog) {
-                  this.pDialog = this.loadFragment({
-                    name: "sap.training.exc.view.Dialog"
-                  }).then(function (oDialog) {
-                    syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
-                    return oDialog;
-                  }.bind(this));
-                }
-                this.pDialog.then(function (oDialog) {
-                  oDialog.open();
-                });
-              },
-              onCloseDialog: function () {
-                this.byId("dialog").close();
-              },
-              onCustomerChange: function (oEvent) {
-                var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
-                this.byId("bookingTable").setBindingContext(oBindingContext);
-              },
-              onInit:function(){
+          onInit: function () {
+              var oModel = new JSONModel();
+              this.getView().setModel(oModel, "customer");
+          },
 
-                
-                var oModel= new JSONModel()
-                this.getView().setModel(oModel,'arbaz')
-              },
-              onFilterCustomers: function (oEvent) {
-                var aFilter = [];
-                var sQuery = oEvent.getParameter("query");
-                if (sQuery && sQuery.length > 0) {
+          onSave: function () {
+              var oModelData = this.getView().getModel("customer").getData();
+              var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+              if (oModelData.Discount === undefined) { oModelData.Discount = 0; }
+
+              this.byId("customerTable").getBinding("items").create({
+                  "Form": oModelData.Form,
+                  "CustomerName": oModelData.CustomerName,
+                  "Discount": oModelData.Discount + "", //Values for property 'Discount' must be quoted in the payload
+                  "Street": oModelData.Street,
+                  "PostCode": oModelData.PostCode,
+                  "City": oModelData.City,
+                  "Country": oModelData.Country,
+                  "Email": oModelData.Email,
+                  "Telephone": oModelData.Telephone
+              }).created().then(function () {
+                  MessageToast.show(oResourceBundle.getText("customerCreatedMessage"));
+              });
+          },
+
+          onCloseDialog: function () {
+              // note: You don't need to chain to the pDialog promise, since this event handler
+              // is only called from within the loaded dialog itself.
+              this.byId("dialog").close();
+          },
+
+          onCustomerChange: function (oEvent) {
+              var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+              this.byId("bookingTable").setBindingContext(oBindingContext);
+          },
+
+          onFilterCustomers: function (oEvent) {
+              // build filter array
+              var aFilter = [];
+              var sQuery = oEvent.getParameter("query");
+              if (sQuery && sQuery.length > 0) {
                   aFilter.push(new Filter("CustomerName", FilterOperator.Contains, sQuery));
-                }
-              
-                var oTable = this.byId("customerTable");
-                var oBinding = oTable.getBinding("items");
-                oBinding.filter(aFilter);
               }
-             
 
-          
-            
+              // filter binding
+              var oTable = this.byId("customerTable");
+              var oBinding = oTable.getBinding("items");
+              oBinding.filter(aFilter);
+          }
 
-
-        });
-    });
+      });
+  });
